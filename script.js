9daa379313834ae1f4a3a1de26554e284895d444
KnifeParty12 <flashfunk01@gmail.com>
2019-05-26 00:45:17 +0530

    Complete Script
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-let filterInput = document.getElementById('filterinput');
+let filterInput = document.getElementById('filterInput');
 
 // Add Event Listener
 filterInput.addEventListener('keyup',filterNames);
 
 function filterNames() {
-    console.log(1);
+    //Get Value of Input
+
+    let filterValue = document.getElementById('filterInput').value.toUpperCase();
+
+    //Get names UL
+    let ul = document.getElementById('names');
+
+    //Get li from ul
+
+    let li = ul.querySelectorAll('li.collection-item');
+
+    // Loop Through collection item li
+    for(let i=0; i < li.length; i++){
+        let a = li[i].getElementsByTagName('a')[0];
+        if(a.innerText.toUpperCase().indexOf(filterValue)> -1){
+                li[i].style.display = '';
+        } else{
+            li[i].style.display = 'none';
+        }
+    }
 }
