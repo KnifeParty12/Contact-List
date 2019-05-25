987beee2891833ca968188b8d004b867dfcf34ed
KnifeParty12 <flashfunk01@gmail.com>
2019-05-26 00:09:36 +0530

    Layout Complete
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

---
+++ script.js
@@ -0,0 +1,8 @@
+let filterInput = document.getElementById('filterinput');
+
+// Add Event Listener
+filterInput.addEventListener('keyup',filterNames);
+
+function filterNames() {
+    console.log(1);
+}
